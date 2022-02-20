45f6b36a5119ccb227abdb2d0325b58a44d60662
hotdogxhd <100070293+hotdogxhd@users.noreply.github.com>
2022-02-20 21:40:04 +0800

    pull commit

--- src/main/java/com/xhd/GitTest.java
+++ src/main/java/com/xhd/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master Commit");
         System.out.println("hot-fix Commit ");
         System.out.println("push commit");
-        System.out.println("ssh commit2");
+        System.out.println("ssh commit2");、
+        System.out.println("pull commit");、
     }
 }
