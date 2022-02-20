347d7286739bcca61116c4a51cc551af49dc2131
xhd <192243498@qq.com>
2022-02-20 21:18:37 +0800

    push commit

--- src/main/java/com/xhd/GitTest.java
+++ src/main/java/com/xhd/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello Git4");
         System.out.println("master Commit");
         System.out.println("hot-fix Commit ");
+        System.out.println("push commit");
     }
 }
